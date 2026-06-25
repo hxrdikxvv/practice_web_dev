3a02c13dca75027350ba14b12cf52353b27b6146
hardik_negi <hknegi317@gmail.com>
2026-06-25 10:47:04 +0530

    basics of Javascript along with semantic html.

---
+++ javascript/basics.js
@@ -0,0 +1,68 @@
+// Variable - Used to store data in JS
+
+// let name="Hardik";
+
+// 3 ways to create variables
+
+// 1.var
+// older way of declaring variables.
+// var is mostly avoided in modern JavaScript because it behaves differently with scope and can create bugs.
+
+
+// 2.let
+
+// let age=19;
+// used when values can change later
+
+
+// 3.const
+// used when values cannot change later.
+// const nationality="Indian";
+
+
+
+// Mostly camelCase is used for naming variables like firstName,totalMarks,userAge.
+
+
+// variables can be reassigned with with let but not with const.
+
+
+
+
+ 
+// Var is function scope , it ignores blocks like if,for,etc.
+// if (true) {
+//     var name="Hardik";
+// }
+
+// console.log(name);
+
+// for (var i = 0; i < 5; i++) {
+
+// }
+
+// console.log(i);
+
+
+// let is block scoped 
+
+
+
+
+
+
+
+// Data types
+
+
+// operators
+
+
+// conditionals and loops
+
+
+// functions
+
+
+// scope and hoisting
+
